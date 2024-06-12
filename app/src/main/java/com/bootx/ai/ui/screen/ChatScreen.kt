720a3bc83e1a10aa0c70f430d53685eb3de804b5
black <1169794338@qq.com>
2024-06-12 17:58:39 +0800

    修改包名 加入网络请求

--- app/src/main/java/com/bootx/ai/ui/screen/ChatScreen.kt
+++ app/src/main/java/com/bootx/ai/ui/screen/ChatScreen.kt
@@ -1,16 +1,41 @@
 package com.bootx.ai.ui.screen
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
 import com.bootx.ai.ui.viewmodal.HomeModel
-
 
 @Composable
 fun ChatScreen(
     navController: NavController,
     homeModel: HomeModel = viewModel()
 ) {
-    Text(text = "ChatScreen")
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp)
+    ) {
+        item {
+            Row {
+                AsyncImage(
+                    contentScale = ContentScale.FillBounds,
+                    modifier = Modifier.clip(CircleShape),
+                    model = "https://cdn.aichatzw.com/home/assets/logo-a42cf706.png",
+                    contentDescription = ""
+                )
+                Text(text = "sadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfassadfasfas")
+            }
+        }
+    }
 }
