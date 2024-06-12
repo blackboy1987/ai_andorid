0c71af4d72ede70d30ad245f58e060ed059d38a8
black <1169794338@qq.com>
2024-06-12 08:14:08 +0800

    init

---
+++ app/src/main/java/com/bootxai/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.bootxai
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+import com.bootxai.ui.screen.MainScreen
+import com.bootxai.ui.theme.AppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        setContent {
+            AppTheme {
+                MainScreen()
+            }
+        }
+    }
+}
